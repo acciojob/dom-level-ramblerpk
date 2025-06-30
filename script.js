b39634cbeb8d6ef2ef5a83da16544a083b706d93
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-06-30 21:02:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let element = document.getElementById("level");
+
+let counter = 0;
+
+while(element) {
+    counter ++;
+    if (element === document.documentElement) {
+        break;
+    }
+    element = element.parentNode;
+}
+alert(`The level of the element is: ${levelCount}`);
